f9bef6c52aafff243df13c282664413763a814ad
VidyaRaghuraman <32456909+VidyaRaghuraman@users.noreply.github.com>
2018-06-04 10:13:50 +0100

    Add files via upload

---
+++ Assertions.java
@@ -0,0 +1,21 @@
+public class Assertions {
+
+    public static String AssertName() {
+        String assertname = "FB User qa-test-997";
+        return assertname;
+    }
+
+    public static String AssertMail() {
+        String assertmail = "qa-test-997@your.md";
+        return assertmail;
+    }
+
+
+    public static String AssertContentType() {
+        String assertcontenttype = "qa-test-997@your.md";
+        return assertcontenttype="application/json;charset=UTF-8";
+    }
+
+}
+
+
---
+++ Resources.java
@@ -0,0 +1,27 @@
+public class Resources {
+    public static String AuthRes(){
+        String authres="/auth/authenticate";
+        return authres;
+
+    }
+
+    public static String AuthCheckRes(){
+        String authcheckres="/auth/check";
+        return authcheckres;
+
+    }
+
+    public static String GetProfileRes(){
+        String getprofileres="/profiles/";
+        return getprofileres;
+
+    }
+
+    public static String GetUserMainRes(){
+        String getusermainres="/profiles/";
+        return getusermainres;
+
+    }
+
+
+}
